4b0ae3f70660445d13f9a29e3666d1b833c073ce
avbelyshev <avbelishev@gmail.com>
2017-12-26 00:03:58 +0300

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'meetingfriends.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'https://meetingfriends.herokuapp.com'}
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
